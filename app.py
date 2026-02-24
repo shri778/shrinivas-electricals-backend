e3f0ee028a40d60f1c29b2dfd59d2536ed465c14
shri778 <archanagadwe81@gmail.com>
2026-02-24 11:38:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,3 @@ def chat():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Uses Render PORT if available
     app.run(host="0.0.0.0", port=port)
-    return jsonify(response.json())
