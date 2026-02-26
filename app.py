50e1ef559d597fda7c081d61f0b192358132bc35
shri778 <archanagadwe81@gmail.com>
2026-02-26 10:07:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -11,17 +12,16 @@ def home():
 @app.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
-    print("DEBUG: Received data =", data)
-    
+    print("DEBUG: Received data =", data)  # temporary debug
+
     if not data or "message" not in data:
-        
         return jsonify({"reply": "No message received"}), 400
-    
+
     user_message = data["message"]
     reply = "You said: " + user_message
-    
+
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
