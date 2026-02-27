f2a563976d741e887223c4e0d23b03a5e488aa48
shri778 <archanagadwe81@gmail.com>
2026-02-27 10:55:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,10 +26,6 @@ def chat():
     except Exception as e:
         return jsonify({"reply": "Error occurred"}), 500
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 10000))
